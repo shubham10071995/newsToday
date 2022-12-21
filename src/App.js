a38f250238badfb97e48f86ea087734a25976abc
shubham <spatil10795@gmail.com>
2022-12-21 18:53:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [news,setNews] = useState([])
+  useEffect(()=>{
+      axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=70bb0b52dcdc4be7b0754f55f8542bed").then((res)=>{
+        console.log(res.data.articles);
+        setNews(res.data.articles)
+      })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container my-5">
+      
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="https://clipground.com/images/png-latest-news-update-6.png" alt="Bootstrap" width="1000" height="350"/>
+    </a>
+  </div>
+
+        <h1 className="Heading">Hello NewsSeekers</h1>
+
+
+        <div className="row text-center">
+          {
+            news.map((val)=>{
+              
+              return(
+                <div className="col">
+          <div className="card" style={{width: "50px;"}}>
+  <img src={val.urlToImage} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{val.title
+}</h5>
+    <p className="card-text">{val.description
+
+}</p>
+{/* <button type="button" class="btn btn-primary">{val.url}</button> */}
+<a href={val.url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read more</a>
+  </div>
+</div>
+          </div>
+              )
+            })
+          }
+          
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
